ec0f7b79191d53c75664d0e73807964b82b45add
Matthew Teece <mteece@pixelmedia.com>
2014-04-27 09:31:59 -0400

    Mock based unit test with AFNetworking; testing discovery.

--- Project/Public Works/Public WorksTests/DataSources/HTTP/PBWKSHttpClientTest.m
+++ Project/Public Works/Public WorksTests/DataSources/HTTP/PBWKSHttpClientTest.m
@@ -10,7 +10,9 @@
 #import "PBWKSHttpContext.h"
 #import "AFHTTPSessionManager.h"
 
+#import <AFNetworking/AFNetworking.h>
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
 
 @interface PBWKSHttpClientTest : XCTestCase
 
@@ -122,6 +124,56 @@
     while (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_NOW))
         [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
                                  beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+
+}
+
+- (void)testHttpClientWillGetResponseForDiscovery
+{
+    // create a mock of the AFHTTPClient:
+    id mockClient = [OCMockObject mockForClass:[AFHTTPSessionManager class]];
+    
+    // mock data
+    NSData *mockData = [@"{ \"changeset\":\"2011-02-03 14:18\", \"contact\":\"You can email or call for assistance api@mycity.org +1 (555) 555-5555\", \"key_service\":\"You can request a key here: http://api.mycity.gov/api_key/request\", \"endpoints\":[ { \"specification\":\"http://wiki.open311.org/GeoReport_v2\", \"url\":\"http://open311.mycity.gov/v2\", \"changeset\":\"2010-11-23 09:01\", \"type\":\"production\", \"formats\":[ \"text/xml\" ] }, { \"specification\":\"http://wiki.open311.org/GeoReport_v2\", \"url\":\"http://open311.mycity.gov/test/v2\", \"changeset\":\"2010-10-02 09:01\", \"type\":\"test\", \"formats\":[ \"text/xml\", \"application/json\" ] }, { \"specification\":\"http://wiki.open311.org/GeoReport_v3\", \"url\":\"http://open311.mycity.gov/v3\", \"changeset\":\"2011-02-03 14:18\", \"type\":\"test\", \"formats\":[ \"text/xml\", \"application/json\" ] } ] }" dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSError *error;
+    NSDictionary *mockJSON = [NSJSONSerialization JSONObjectWithData:mockData options:0 error:&error];
+    NSLog(@"JSON DIct: %@", mockData);
+    
+    [[[mockClient expect] andDo:^(NSInvocation *invocation) {
+        
+        // we define the sucess block:
+        void (^successBlock)(AFHTTPRequestOperation *operation, id responseObject) = nil;
+        
+        // Using NSInvocation, we get access to the concrete block function
+        // that has been passed in by the actual test
+        // the arguments for the actual method start with 2 (see NSInvocation doc)
+        [invocation getArgument:&successBlock atIndex:4];
+        
+        // now we invoke the successBlock with some "JSON"...:
+        successBlock(nil,
+                     [NSDictionary dictionaryWithDictionary:mockJSON]
+                     );
+        
+    }] POST:[OCMArg any] parameters:nil success:[OCMArg any] failure:[OCMArg any]];
+    
+    [mockClient POST:@"discovery.json" parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        XCTAssertEqualObjects(
+                             @"2011-02-03 14:18",
+                             [responseObject objectForKey:@"changeset"],
+                             @"Mock JSON"
+                             );
+        // Get the 'results' array
+        if ([[responseObject objectForKey:@"endpoints"] isKindOfClass:[NSArray class]])
+        {
+            NSArray *array = [responseObject objectForKey:@"endpoints"];
+            NSLog(@"results array: %@", array);
+            XCTAssertEqual(3, [array count], @"Array should contain 3 object.");
+        }
+        
+        
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        XCTFail(@"Mock JSON failed.");
+    }];
 
 }
 
